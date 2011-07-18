0234ec16ce8973d7ccc9abd883bfb0d332be73d2
Danny Greg <danny@dannygreg.com>
2011-07-18 08:47:56 +0100

    Add in the file templates to both the repo and the BlockPlayground project.

---
+++ Source/NSUndoManager-BKAdditions.h
@@ -0,0 +1,12 @@
+//
+//  NSUndoManager-BKAdditions.h
+//  BlockKit
+//
+//  Created by Danny Greg on 18/07/2011.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSUndoManager (NSUndoManager_BKAdditions)
+
+@end
---
+++ Source/NSUndoManager-BKAdditions.m
@@ -0,0 +1,12 @@
+//
+//  NSUndoManager-BKAdditions.m
+//  BlockKit
+//
+//  Created by Danny Greg on 18/07/2011.
+//
+
+#import "NSUndoManager-BKAdditions.h"
+
+@implementation NSUndoManager (BKAdditions)
+
+@end
